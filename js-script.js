22b33a12ae66fa13dfb5ecd3f608d6ffa80854f3
yannick-nkemto-nkenja! <yanniknkemtonkenja@libero.it>
2020-01-31 10:32:55 +0100

    contain all the index and string

---
+++ js-script.js
@@ -0,0 +1,16 @@
+var username = prompt("Enter your name here");
+var password = prompt("Enter Password");
+if( password==220891 )
+{
+    if (username == "BELLA") 
+{
+    console.log('you logged in');
+        
+    }
+    else if  (username == "Yannick" )
+    {
+
+        console.log('you logged in');
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,62 @@
+const STREET_NAME = 'Via Samone'
+const STREET_NUMBER = '2'
+const ZIP_CODE = '10010'
+const CITY_NAME = 'Banchette'
+const PROVINCE_NAME = 'Torino'
+const REGION_NAME = 'Piemonte'
+const COUNTRY_NAME = 'Italy'
+
+
+let MYADDRESS = STREET_NAME +' ' + STREET_NUMBER + '\n'+ ZIP_CODE + '\n'+ CITY_NAME + '\n'+ PROVINCE_NAME +'\n'+ COUNTRY_NAME 
+
+
+//const MYADDRESS = 'Via Samone 2, 10010\nBanchette (TO)\nTorino \nPiemonte\nItaly'
+
+// console.log (MYADDRESS)
+
+
+const SWEET_LIFE = 'It Is Good To be Happy All Time'
+const ENJOY_LIFE = 'Life Is To Short'
+const DO_NOT_ENVY = 'Do Not Envy Someone Because He is Succeeding'
+const LEARN_TO_GROW = 'there is no Age in Learning'
+
+let MYLIFESTORY = SWEET_LIFE + '\n' + ENJOY_LIFE +'\n' + DO_NOT_ENVY + '\n' + LEARN_TO_GROW
+ 
+
+//let admin=''
+//let name ='John';
+//admin = name ;
+//alert (admin) ;
+
+//let earth;
+//let visitor;
+
+const newAddress = `${STREET_NAME}${STREET_NUMBER} 
+${CITY_NAME} 
+${PROVINCE_NAME} 
+${REGION_NAME}
+${COUNTRY_NAME}`
+
+
+console.log(newAddress)
+
+
+const FULL_NAME = `Nkemto Nkenja Yannick`
+const MYSTREET_ADDRESS = `Via Samone 2`
+const POSTAL_CODE = `10010`
+const TOWN_NAME = `Banchette`
+const WORK_EXPIRIENCE =`Help Cook, Cameriere, Chief Cook, Pizzaiolo`
+const STUDY = `Advance Level, licenza media`
+const LANGUAGES = `Italian, English, French, Spanish,`
+const SKILLS = `I work as an help cook at a restaurant called la Rava e la Fava
+where i was responsible for the first plate and the oven \n Then i work in a canteen of the Diocesan caritas of fraternity`
+
+const CV = `${FULL_NAME}
+${MYSTREET_ADDRESS}
+${POSTAL_CODE}, ${TOWN_NAME}
+${WORK_EXPIRIENCE}
+${STUDY}
+${LANGUAGES}
+${SKILLS}`
+
+console.log (CV)
