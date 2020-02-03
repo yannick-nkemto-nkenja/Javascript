aad9421515854eeb1b97f109fc6c7ff606cea1eb
yannick-nkemto-nkenja! <yanniknkemtonkenja@libero.it>
2020-02-03 15:14:00 +0100

    conditional-statement

---
+++ conditional-statements.js
@@ -0,0 +1,12 @@
+let s = Math.floor(Math.random() + (10 - 1 ))
+let bella = prompt('guess a number') 
+
+if(s == bella)  {
+    alert('number was correct')
+} else {
+    alert('the number was incorrect the correct number was ' + s)
+}
+
+
+let b = 1;
+let d = 2;
