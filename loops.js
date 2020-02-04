8951cbf58ac57417f65a2019cbe90d09929ce6a7
yannick-nkemto-nkenja! <yanniknkemtonkenja@libero.it>
2020-02-04 13:08:43 +0100

    loops.Math

---
+++ loops.js
@@ -0,0 +1,31 @@
+//let i = 0;
+//while (i < 3) {
+//   console.log(i);
+//   i++;
+//}
+
+
+//let i = 3;
+
+
+//while (i > 0) {
+//    console.log(i);
+//    i--;
+//}
+
+
+
+let n = 0;
+let word = 'Na';
+
+
+let jointworld = ""
+let lastword = "  batman!"
+
+while (n<= 16) {
+    word+="Na";
+    n++
+
+}
+   
+console.log(word+lastword)
