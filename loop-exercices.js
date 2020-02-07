8d4d06cfeed649841eaa60cf23de7e927ae31349
yannick-nkemto-nkenja! <yanniknkemtonkenja@libero.it>
2020-02-07 08:56:12 +0100

    loops-exercices

---
+++ loop-exercices.js
@@ -0,0 +1,92 @@
+// let num = prompt ('enter a number greater than 100');
+// while (num && num <= 100) {
+//     num = prompt('please try again.');
+//     while (num && num <= 100);
+     
+// } 
+// console.log('well done! is correct');
+ 
+//  for (
+//      let num = prompt('put a number greater than 100');
+//      num && num <= 100;
+//      num = prompt('please is not correct.')
+//  ) {}
+//  console.log('great you got it!');
+
+
+// let n = 10;
+// for (let i = 2; i <= n; 1++) {
+//     for (let j = 2; j < i; j++) {
+//     if (i % j ==0);
+//     }
+// }
+//  console.log(i);
+
+
+//  let x;
+//  for (var i = 0; i < 5; i++) {
+//         x= "*";
+//  for (var j = 0; j < 5; j++) {
+//         x+=x;
+//  }
+//  console.log('x');
+// }
+//  console.log( x )
+
+
+// let i = 3;
+// while (i > 0 ) {
+//     console.log(i);
+//     i--
+// }
+
+// var enterNumber = prompt ("prime number from 2 to n");
+// for (var n =2; i <=2 ; n++) {
+//     console.log(n);
+//     n++
+// }
+
+// let n = 2;
+// for(i = 1; n >= 2; i++) {
+//     while(n > divisor) {
+//         if( n % divisor ==2);
+//     }
+//     return true
+// }
+   
+
+
+
+
+// let n = 10;
+// for (i = 2; i < n; i++) {
+//     let prime = true;
+
+//     for( j = 2; j < i; j++){
+//         if(i % j == 0) {
+//             prime = false;
+  
+//         }
+    
+//     }
+
+//     if(prime ) console.log (i)
+// }
+
+// print all even numbers from 2 to 100
+
+// var a = 2;
+// for (a = 2; a <= 100; a++) {
+//      if(a % 2 == 0) {
+//           console.log (a)
+//      }
+// }
+
+
+
+// print all multiples of 20 from 0 to 100
+
+// let m = 20;
+// for(let i = 0; i <= 5; i = i + 1) {
+//      console.log ( i * m);
+// }
